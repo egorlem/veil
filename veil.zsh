81e8fd5b038ef5a8579c8ea0e15baf4a163a7051
Egor Lem <torridtime@gmail.com>
2025-11-20 13:36:13 +0300

    Initial commit

---
+++ veil.zsh
@@ -0,0 +1,13 @@
+# Veil – https://github.com/egorlem/veil
+#
+# Modular Z Shell Configuration System
+# Takes full control of zsh configuration through logical modules
+# ------------------------------------------------------------------------------
+# License: WTFPL – https://github.com/egorlem/veil/blob/main/LICENSE 
+# ------------------------------------------------------------------------------
+# Authors
+# -------
+#
+#  * Egor Lem <guezwhoz@gmail.com> / egorlem.com
+#
+# ------------------------------------------------------------------------------
